8f80dcf87f117f1e017363473bbf19e926aa1ba4
Dariia <grimalskayad@gmail.com>
2020-12-22 13:50:03 +0200

    Small db changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function createSalesBlock() {
+function createSalesBlock(text1, text2, text3, text4) {
     const sales = document.createElement('div')
     sales.classList.add('sales-block')
-    sales.innerHTML = '<a href="#" class="sale sale-one"></a><a href="#" class="sale sale-two"></a>'
-    sales.innerHTML += '<a href="#" class="sale sale-three"></a><a href=#" class="sale sale-four"></a>'
+    sales.innerHTML = `<a href="#" class="sale sale-one">${text1}</a><a href="#" class="sale sale-two">${text2}</a>`
+    sales.innerHTML += `<a href="#" class="sale sale-three">${text3}</a><a href=#" class="sale sale-four">${text4}</a>`
     return sales
 } 
 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const main = document.querySelector('.main__inner')
-main.appendChild(createSalesBlock())
+
 async function getInfo() {
     const response = await fetch('https://my-json-server.typicode.com/dariiagrim/fruitsLab/sales', {method: "GET", headers: {"Content-Type":"application/json"}})
     const data = await response.json()
-    console.log(data.url)
+    main.appendChild(createSalesBlock(data.sales[0].text, data.sales[1].text, data.sales[2].text, data.dales[3].text))
 }
 getInfo()
