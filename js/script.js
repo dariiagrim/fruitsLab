43809d96c0f5110c6d46e8da2cf39d999037b595
Dariia <grimalskayad@gmail.com>
2020-12-22 13:50:45 +0200

    -----

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ const main = document.querySelector('.main__inner')
 async function getInfo() {
     const response = await fetch('https://my-json-server.typicode.com/dariiagrim/fruitsLab/sales', {method: "GET", headers: {"Content-Type":"application/json"}})
     const data = await response.json()
-    main.appendChild(createSalesBlock(data.sales[0].text, data.sales[1].text, data.sales[2].text, data.dales[3].text))
+    main.appendChild(createSalesBlock(data[0].text, data[1].text, data[2].text, data[3].text))
 }
 getInfo()
