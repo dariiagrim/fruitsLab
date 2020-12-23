956fed02a01cf537dac69d698f8aa1846e865602
Dariia <grimalskayad@gmail.com>
2020-12-23 18:43:41 +0200

    ---

--- js/fruitsByType.js
+++ js/fruitsByType.js
@@ -1,4 +1,5 @@
 const sweetLink = document.querySelector('.nav__sweet')
+const sourLink = document.querySelector('.nav__sour')
 const allLink = document.querySelector('.nav__all')
 
 sweetLink.onclick = function(e) {
@@ -12,6 +13,18 @@ sweetLink.onclick = function(e) {
     renderSweetFruits()
 }
 
+
+sourLink.onclick = function(e) {
+    e.preventDefault()
+    let href = window.location.href
+    href = href.slice(0, href.length - window.location.hash.length)
+    if (href[href.length - 1] === '/') {
+        href = href.slice(0, href.length - 1)
+    }
+    window.history.replaceState( null , null, href + '/#sour');
+    renderSourFruits()
+}
+
 async function renderSweetFruits() {
     main.innerHTML = ''
     const sweetReq = await fetch('https://my-json-server.typicode.com/dariiagrim/fruitsLab/sweet', {method: "GET", headers: {"Content-Type":"application/json"}})
@@ -21,5 +34,16 @@ async function renderSweetFruits() {
         sweetContainer.appendChild(createProduct(dataSweet[i].url, dataSweet[i].name, dataSweet[i].price, 'sweet'))
     }
     main.appendChild(sweetContainer)
+}
+
+async function renderSourFruits() {
+    main.innerHTML = ''
+    const sourReq = await fetch('https://my-json-server.typicode.com/dariiagrim/fruitsLab/sour', {method: "GET", headers: {"Content-Type":"application/json"}})
+    const dataSour = await sourReq.json()
+    const sourContainer = createProductContainer('sour__container')
+    for (let i = 0; i < 6; i++) {
+        sourContainer.appendChild(createProduct(dataSour[i].url, dataSour[i].name, dataSour[i].price, 'sour'))
+    }
+    main.appendChild(sourContainer)
 }
 
