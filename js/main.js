128f1f960f2c42a443b2bfc036f3faf44ac599fe
Dariia <grimalskayad@gmail.com>
2020-12-23 22:28:39 +0200

    Check commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function createSalesBlock(text1, text2, text3, text4) {
     const sales = document.createElement('div')
     sales.classList.add('sales-block')
     sales.innerHTML = `<a href="#" class="sale sale-one">${text1}</a><a href="#" class="sale sale-two">${text2}</a>`
-    sales.innerHTML += `<a href="#" class="sale sale-three">${text3}</a><a href=#" class="sale sale-four">${text4}</a>`
+    sales.innerHTML += `<a href="#" class="sale sale-three">${text3}</a><a href="#" class="sale sale-four">${text4}</a>`
     return sales
 } 
 
---
+++ js/someLinks.js
@@ -0,0 +1,29 @@
+window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('sale')) {
+        console.log(5)
+        renderSalePage()
+    }
+    if (event.target.classList.contains('buy-sale')) {
+        window.location.href = window.location.href.slice(0, window.location.href.length - 1)
+        renderAll()
+    }
+})
+
+async function renderSalePage() {
+    main.innerHTML = ""
+    if (main.parentNode.parentNode.classList.contains('one-page-main')) {
+        main.parentNode.parentNode.classList.remove('one-page-main')
+    }
+    const salesReq = await fetch('https://my-json-server.typicode.com/dariiagrim/fruitsLab/sales', {method: "GET", headers: {"Content-Type":"application/json"}})
+    const data = await salesReq.json()
+    for (let i = 0; i < 4; i++) {
+        main.appendChild(createBigSale(data[i].text))
+    }
+}
+
+function createBigSale(saleDescription) {
+    const saleBlock = document.createElement('div')
+    saleBlock.classList.add('big-sale-block')
+    saleBlock.innerHTML = `<p class="sale-description">${saleDescription}</p><button class="buy-sale">Придбати фрукти</button>`
+    return saleBlock
+}
